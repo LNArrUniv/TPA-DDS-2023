fc431c6db1b2985a956998b73d2b256fc2d6b6e6
DanJudzik7 <djudzik@frba.utn.edu.ar>
2023-10-19 18:07:07 -0300

    avances csv (necesito ayuda con css xd)

--- src/main/java/ar/edu/utn/frba/dds/Persistencia/repositorios/RepositorioEntidades.java
+++ src/main/java/ar/edu/utn/frba/dds/Persistencia/repositorios/RepositorioEntidades.java
@@ -1,8 +1,11 @@
 package ar.edu.utn.frba.dds.Persistencia.repositorios;
 
 import ar.edu.utn.frba.dds.Modelos.Entidad;
+import ar.edu.utn.frba.dds.Persistencia.EntityManagerHelper;
 import ar.edu.utn.frba.dds.Persistencia.repositorios.daos.DAO;
 import ar.edu.utn.frba.dds.Persistencia.repositorios.daos.DAOHibernate;
+
+import java.util.List;
 
 public class RepositorioEntidades extends Repositorio<Entidad> {
   private static RepositorioEntidades instance = null;
@@ -17,4 +20,8 @@ public class RepositorioEntidades extends Repositorio<Entidad> {
     }
     return instance;
   }
+  public List getEntidades(){
+    return EntityManagerHelper.createQuery("from Entidad").getResultList();
+  }
 }
+
